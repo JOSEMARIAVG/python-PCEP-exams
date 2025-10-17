5a735b434857691fe768be4400e6742eff468144
JOSEMARIAVG <josemvg14@gmail.com>
2025-10-17 23:24:02 +0200

    4 QUESTIONS

---
+++ TEST_PYTHON_PCEP.py
@@ -0,0 +1,162 @@
+#------------------------------------------------------------------------------------------------------
+# QUESTION 1
+# Code:
+
+list = [False, True, "2", 3, 4, 5]
+# -> Insert Code Here
+print(b)
+
+# Options:
+b = 0 not in list #1
+b = list[0] #2
+b = 0 in list #3 
+b = False #4
+
+# CORRECT OPTION: 3 - 0 considered as False. And False option is present in list
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 2
+# Assuming that the tuple is a correctly created tuple, the fact that tuples are immutable means that the following instruction:
+my_tuple[1] = my_tuple[1] + my_tuple[0]
+
+# Options:
+Is illegal #1
+May be illegal if the tuple contains strings #2
+Can be executed if and only if the tuple contains at least two elements #3
+Is fully correct #4
+
+# CORRECT OPTION: 3 - tuples are immutable, which means you cannot change their elements after creation
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 3
+# What is the expected output of the following code?
+
+x = [0,1,2]
+x.insert(0,1)
+del x[1]
+print(sum(x))
+
+# Options:
+2  #1
+4  #2
+5  #3
+3  #4
+
+# CORRECT OPTION: 2 - Insert(index,value)
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 4
+# What is the expected output of the following code?
+
+list1 = [1,3]
+list2 = list1
+list1[0] = 4
+print(list2)
+
+# Options:
+[1,3]  #1
+[1,4]  #2
+[4,3]  #3
+[1,3,4]  #4
+
+# CORRECT OPTION: 3 - list2 refers to the same list as list1
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
+
+#------------------------------------------------------------------------------------------------------
+# QUESTION 
+# 
+
+# Options:
+  #1
+  #2
+  #3
+  #4
